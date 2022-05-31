db24f549e2d8b9d2d28ad402eeff0e35f8042a1c
Renan <baratellarenan@gmail.com>
2022-05-31 15:32:47 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 03

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.model.Tema;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 
 @RestController
@@ -29,6 +31,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -61,10 +66,21 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		if(temaRepository.existsById(postagem.getTema().getId())) {
+			return ResponseEntity.ok(postagemRepository.save(postagem));
+		}
+		else
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
-	
-	
+		
+		
+//		@PostMapping
+//		public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem){
+//			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+//		}
+			
+			
+			
 	
 //	@PutMapping
 //	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
@@ -74,24 +90,46 @@ public class PostagemController {
 //			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 //	}
 	
+//	@PutMapping
+//	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
+//		return postagemRepository.findById(postagem.getId())
+//				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+//				.orElse(ResponseEntity.notFound().build());
+//	}
+	
 	@PutMapping
 	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.notFound().build());
+		if(postagemRepository.existsById(postagem.getId()) && temaRepository.existsById(postagem.getTema().getId())) {
+			return ResponseEntity.ok(postagemRepository.save(postagem));
+		}
+		else
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
-		if(postagemRepository.existsById(id)) {
-			postagemRepository.deleteById(id);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-		}
-		else
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> {
+					postagemRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 	
+	
+	
+	
+	//feito por mim
+//	@DeleteMapping("/{id}")
+//	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+//		if(postagemRepository.existsById(id)) {
+//			postagemRepository.deleteById(id);
+//			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+//		}
+//		else
+//			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//	}
 	
 }
 
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,17 +14,19 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "tb_postagens") 
+@Table(name = "tb_postagens")
 public class Postagem {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	private Long id; 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!") 
+	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!")
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-	private String titulo; 
+	private String titulo;
 
 	@NotNull(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
@@ -31,6 +34,10 @@ public class Postagem {
 
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public Long getId() {
 		return id;
@@ -62,6 +69,14 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
