5da7e1aa7c1a368b5e0b6d3e3716dd187ca28133
Renan <baratellarenan@gmail.com>
2022-07-20 19:21:41 -0300

    Projeto finalizado

--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -33,9 +33,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/**").permitAll() 
+//		.antMatchers("/**").permitAll() 
 		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll() 
+//		.antMatchers(HttpMethod.GET ,"/usuarios/*").permitAll()
 		.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
 		.antMatchers(HttpMethod.GET ,"/tema").permitAll()
 		.anyRequest().authenticated()
